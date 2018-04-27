b88870cf021e14d954039ac0856be9352c3c9687
g. nicholas d'andrea <gnidan@users.noreply.github.com>
2018-04-26 22:31:42 -0400

    Initial sphinx commit

---
+++ push.sh
@@ -0,0 +1,110 @@
+#!/usr/bin/env bash
+# Push HTML files to gh-pages automatically.
+
+# Fill this out with the correct org/repo
+ORG=trufflesuite
+REPO=proposal-artifacts-format
+# This probably should match an email for one of your users.
+NAME="Travis Deployer"
+EMAIL="no-reply@trufflesuite.com"
+
+set -e
+
+DRY_RUN=
+if [ "$1" != "dry" ]; then
+  DRY_RUN="true"
+fi
+
+
+###############################################################################
+# authenticate deployment
+###############################################################################
+
+setup_gh () {
+  openssl aes-256-cbc \
+    -K $encrypted_ed68d7c9ebd1_key \
+    -iv $encrypted_ed68d7c9ebd1_iv \
+    -in deploy-key.enc \
+    -out deploy-key -d
+
+  chmod 600 deploy-key
+  eval `ssh-agent -s`
+  ssh-add deploy-key
+  git config user.name "${NAME}"
+  git config user.email "${GIT_EMAIL}"
+}
+
+
+###############################################################################
+# clone gh-pages branch
+###############################################################################
+
+clone_gh_pages () {
+  # Clone the gh-pages branch outside of the repo and cd into it.
+  cd ..
+  git clone -b gh-pages "git@github.com/$ORG/$REPO.git" gh-pages
+  cd gh-pages
+}
+
+
+###############################################################################
+# copy docs build
+###############################################################################
+
+copy_docs () {
+  local author
+  local branch
+  local directory
+
+  if [ "${TRAVIS_PULL_REQUEST_SLUG}" != "" ];
+  then
+    author=$( dirname "${TRAVIS_PULL_REQUEST_SLUG}" )
+  else
+    author=$( dirname "${TRAVIS_REPO_SLUG}" )
+  fi
+
+  if [ "${TRAVIS_PULL_REQUEST_BRANCH}" != "" ];
+  then
+    branch="${TRAVIS_PULL_REQUEST_BRANCH}"
+  else
+    branch="${TRAVIS_BRANCH}"
+  fi
+
+  # nested directories for authors and branches
+  directory="./${author}/${branch}"
+
+  # ensure output directory exists
+  mkdir -p "${directory}"
+
+  # copy gh-pages output
+  cp -R ../${REPO}/_build/html/* ${directory}
+}
+
+
+###############################################################################
+# add / commit / push
+###############################################################################
+
+publish () {
+  # Add and commit changes.
+  git add -A .
+  git commit -m "Update docs"
+
+  if [ "${DRY_RUN}" != "true" ]; then
+    git push origin gh-pages
+  fi
+}
+
+
+###############################################################################
+# main
+###############################################################################
+
+main () {
+  setup_gh
+  clone_gh_pages
+  copy_docs
+  publish
+}
+
+main
