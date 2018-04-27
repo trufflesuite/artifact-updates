ec4862a79e4c8611f3eb8e08d25f92c7ae18ef43
Travis CI User <travis@example.org>
2018-04-27 02:46:33 +0000

    Update docs

---
+++ trufflesuite/master/searchindex.js
@@ -0,0 +1,1 @@
+Search.setIndex({docnames:["index"],envversion:53,filenames:["index.rst"],objects:{},objnames:{},objtypes:{},terms:{index:0,modul:0,page:0,search:0},titles:["Welcome to Truffle Artifacts Format Proposal\u2019s documentation!"],titleterms:{artifact:0,document:0,format:0,indic:0,propos:0,tabl:0,truffl:0,welcom:0}})
