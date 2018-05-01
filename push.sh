2a1b6c42c4ff0d919842f4d0f19b4101e5274429
g. nicholas d'andrea <gnidan@users.noreply.github.com>
2018-05-01 18:45:12 -0400

    Fix repo

--- push.sh
+++ push.sh
@@ -3,7 +3,7 @@
 
 # Fill this out with the correct org/repo
 ORG=trufflesuite
-REPO=proposal-artifacts-format
+REPO=artifact-updates
 # This probably should match an email for one of your users.
 NAME="Travis Deployer"
 EMAIL="no-reply@trufflesuite.com"
